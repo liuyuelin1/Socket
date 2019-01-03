df98a22cf061ee669437f0af8f034e5cff85a92f
liuyuelin <liuyuelin@autohejia.com>
2019-01-03 16:29:14 +0800

    添加timeout 修改client 为server方式(一对一通信方式),非堵塞模式暂时还未实现

--- python_socket/udp_socket/client.py
+++ python_socket/udp_socket/client.py
@@ -1,13 +1,20 @@
 import socket
 
-address = ('127.0.0.1', 31501)
+aim_address = ('127.0.0.1', 31502)
+self_address = ('127.0.0.1', 31501)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.settimeout(10)
+s.bind(self_address)
 while True:
     msg = raw_input()
     if not msg:
         break
-    s.sendto(msg, address)
+    s.sendto(msg, aim_address)
+    data, addr = s.recvfrom(2048)
+    if not data:
+        print "client has exist"
+        break
+    print "received:", data, "from", addr
 
 s.close()
--- python_socket/udp_socket/server.py
+++ python_socket/udp_socket/server.py
@@ -1,23 +1,24 @@
 import socket
 
+aim_address = ('127.0.0.1', 31501)
+self_address = ('127.0.0.1', 31502)
 
-address = ('127.0.0.1', 31501)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.settimeout(10)
-s.bind(address)
+#s.settimeout(None)
+#s.setblocking(0)
+s.bind(self_address)
 
 while True:
 
     data, addr = s.recvfrom(2048)
 
     if not data:
-
         print "client has exist"
-
         break
 
     print "received:", data, "from", addr
- 
+    s.sendto("send ok!", aim_address)
 
 s.close()
