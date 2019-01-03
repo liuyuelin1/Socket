51ca4e3482fa3f9c06e167d62a6a3e3f64347cd4
liuyuelin <liuyuelin@autohejia.com>
2019-01-03 16:55:51 +0800

    通过设置timeout 及捕获异常达到查询式非堵塞目的,time可以设置为小数

--- python_socket/udp_socket/server.py
+++ python_socket/udp_socket/server.py
@@ -4,16 +4,18 @@ aim_address = ('127.0.0.1', 31501)
 self_address = ('127.0.0.1', 31502)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#s.setblocking(False)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.settimeout(10)
-#s.settimeout(None)
-#s.setblocking(0)
+s.settimeout(0.5)
 s.bind(self_address)
 
 while True:
 
-    data, addr = s.recvfrom(2048)
-
+    try:
+        data, addr = s.recvfrom(2048)
+    except socket.timeout:
+        print "client"
+        continue
     if not data:
         print "client has exist"
         break
